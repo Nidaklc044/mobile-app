d921b4fff97e7dba4200b2d231c45d46b0bf2ae5
Nidaklc044 <nidakilic1707@gmail.com>
2024-05-16 19:44:31 +0300

    Update HikayePage.js

---
+++ HikayePage.js
@@ -0,0 +1,15 @@
+// HikayePage.js
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HikayePage = () => {
+  return (
+    <View>
+      <Text>Hikaye Sayfası</Text>
+      {/* Hikaye sayfasının içeriği buraya eklenebilir */}
+    </View>
+  );
+};
+
+export default HikayePage;
