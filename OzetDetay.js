db9dc645458ffe647c8a6255e9a6349182c00f93
Nidaklc044 <nidakilic1707@gmail.com>
2024-06-08 21:15:56 +0300

    Update OzetDetay.js

--- OzetDetay.js
+++ OzetDetay.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text, TouchableOpacity, ScrollView } from 'react-native';
 import { styles } from './styles';
 
 const OzetDetay = ({ ozet, image, handleBack }) => {
@@ -10,20 +10,22 @@ const OzetDetay = ({ ozet, image, handleBack }) => {
   };
 
   return (
-    <View style={styles.leftPanel}>
-      <TouchableOpacity onPress={handleBack}>
-        <Text style={{ color: 'blue', marginBottom: 10 }}>Geri Dön</Text>
-      </TouchableOpacity>
-      <Image source={image} style={styles.image} resizeMode="cover" />
-      <View style={styles.otherImagesContainer}>
-        <Text style={styles.additionalName}>{showFullSummary ? ozet : ozet.slice(0, 100) + '...'}</Text>
-        {!showFullSummary && (
-          <TouchableOpacity onPress={toggleSummary}>
-            <Text style={{ color: 'blue', marginTop: 5 }}>Devamını Oku</Text>
-          </TouchableOpacity>
-        )}
+    <ScrollView>
+      <View style={styles.leftPanel}>
+        <TouchableOpacity onPress={handleBack}>
+          <Text style={{ color: 'blue', marginBottom: 10 }}>Geri Dön</Text>
+        </TouchableOpacity>
+        <Image source={image} style={styles.image} resizeMode="cover" />
+        <View style={styles.otherImagesContainer}>
+          <Text style={styles.additionalName}>{showFullSummary ? ozet : ozet.slice(0, 100) + '...'}</Text>
+          {!showFullSummary && (
+            <TouchableOpacity onPress={toggleSummary}>
+              <Text style={{ color: 'blue', marginTop: 5 }}>Devamını Oku</Text>
+            </TouchableOpacity>
+          )}
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
